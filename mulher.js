e8852b7b35cf43bc265ff7d75dcbd45e1a372118
maritorezan <marianasueide@hotmail.com>
2023-05-02 19:45:23 -0300

    adiciona a primeira versão do meu projeto

---
+++ mulher.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router() 
+
+const app = express()
+const porta = 3333
+
+const mulheres = [
+    {
+        nome: "Simara Conceição",
+        imagem: "",
+        minibio: "Desenvolvedora e Instrutora"
+    },
+    {
+        nome: "Iana Chan",
+        imagem: "",
+        minibio: "Fundadora da Programaria"
+    },
+    {
+        nome: "Nina da Hora",
+        imagem: "",
+        minibio: "Hacker antirracista"
+    }
+]
+
+function mostraMulheres(request, response) {
+    response.json(mulheres)
+}
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta)
+}
+
+app.use(router.get("/mulheres", mostraMulheres))
+app.listen(porta, mostraPorta)
